f6f5d6192fdf3062fc0eb5c20ec925f725986677
lzustiak <levizustiak@gmail.com>
2024-03-20 23:15:08 -0500

    feat: update user model

--- models/user.go
+++ models/user.go
@@ -1,18 +1,16 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	ID        uint   `json:"id" gorm:"primaryKey"`
-	Name      string `json:"name"`
-	Email     string `json:"email" gorm:"unique"`
-	Password  string
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index"`
+	// ID        uint   `json:"id" gorm:"primaryKey"`
+	Name     string `json:"name"`
+	Email    string `json:"email" gorm:"unique"`
+	Password string
+	// CreatedAt time.Time      `json:"created_at"`
+	// UpdatedAt time.Time      `json:"updated_at"`
+	// DeletedAt gorm.DeletedAt `gorm:"index"`
 }
